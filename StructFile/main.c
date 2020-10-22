1cb36b07ff4532e0003fa8667566b13fc464b8ff
Giovanni Rodriguez G <34606945+grg8881m@users.noreply.github.com>
2020-10-22 09:31:53 -0500

    Add files via upload

---
+++ StructFile/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct personalData
+{
+    char name[20];
+    char lastName[20];
+    int age;
+
+};
+
+int main()
+{
+    printf("Estructuras de Datos\n");
+
+    struct personalData person;
+
+    printf("Leer datos: \n");
+    printf("Ingresar nombre: \n");
+    gets(person.name);
+
+    printf("Ingresar apellido: \n");
+    gets(person.lastName);
+
+    printf("Ingresar edad: \n");
+    scanf("%i", &person.age);
+
+    printf("Imprimir datos: \n");
+    printf("%s \n", person.name);
+    printf("%s \n", person.lastName);
+    printf("%i \n", person.age);
+
+
+    return 0;
+}
